77c3af0514cf8d2c7fc5177c319f0b112c3f4a4f
Danang202 <danangyp@students.amikom.ac.id>
2024-03-05 12:10:48 +0700

    menambahkan fungsi main

--- Calculator.cs
+++ Calculator.cs
@@ -6,7 +6,11 @@ class Calculator
 		int a = 10;
 		int b = 6;
 		
-		Console.WriteLine("Hasil Penambahan: {0} + {1} = {2}"
+		Console.WriteLine("Hasil Penambahan: {0} + {1} = {2}", a, b,Penambahan(a, b));
+		Console.WriteLine("Hasil Pengurangan: {0} - {1} = {2}, a, b, Pengurangan(a, b));
+		
+		Console.WriteLine("\nTekan sembarang key untuk krluar");
+		Console.ReadKey();
     }
 	
 	static int Penambahan(int a, int b)
