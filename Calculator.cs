23f92fc34c7e2d6835de800345fb11efdd023e6d
Danang202 <danangyp@students.amikom.ac.id>
2024-03-05 12:12:58 +0700

    memperbaiki fungsi main

--- Calculator.cs
+++ Calculator.cs
@@ -9,7 +9,7 @@ class Calculator
 		Console.WriteLine("Hasil Penambahan: {0} + {1} = {2}", a, b,Penambahan(a, b));
 		Console.WriteLine("Hasil Pengurangan: {0} - {1} = {2}, a, b, Pengurangan(a, b));
 		
-		Console.WriteLine("\nTekan sembarang key untuk krluar");
+		Console.WriteLine("\nTekan sembarang key untuk keluar");
 		Console.ReadKey();
     }
 	
